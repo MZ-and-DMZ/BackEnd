6184856f98c513aa0e27c96ff4f1cdd019cd8600
choam2426 <choam2426@gmail.com>
2023-10-11 13:49:27 +0900

    add login

--- api.py
+++ api.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
 
+from database import db_client
+from model.user_auth import UserAuth
+from src import login
+from src.config import conf
+
 app = FastAPI()
+client = None
+
+
+@app.on_event("startup")
+async def startup_event():
+    global client
+    client = db_client.mongoClient(conf["DB_server"])
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    global client
+    client.close_client()
 
 
 @app.get(path="/")
 async def root():
     return {"message": "Hello, World!"}
+
+
+# 로그인 API 엔드포인트 정의
+@app.post("/login")
+def auth(user: UserAuth):
+    return login.auth_user(user, client.collection_auth)
---
+++ src/config.py
@@ -0,0 +1,4 @@
+import json
+
+with open("./config/conf.json", "r") as json_file:
+    conf = json.load(json_file)
