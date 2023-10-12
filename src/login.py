978a89e7026babfe370af5d90e745ba22e4e0120
Ohsaem <ss080026@naver.com>
2023-10-12 12:27:25 +0900

    Add Login Hashing

--- src/login.py
+++ src/login.py
@@ -1,12 +1,21 @@
+from .password_hash import verify_password
+
+
 def auth_user(user, collection):
     try:
         result = collection.find_one({"_id": user.id})
-    except:
-        return "server error"
-    if result:
-        if result.get("password") == user.pwd:
-            return {"result": "success"}
-        else:
-            return {"result": "password is incorrect"}
-    else:
+    except Exception as e:
+        return {"result": "server error", "detail": str(e)}
+
+    if result is None:
         return {"result": "user not exist"}
+
+    hashed_password = result.get("password")
+
+    if hashed_password is not None and isinstance(hashed_password, str):
+        hashed_password = hashed_password.encode('utf-8')
+
+    if verify_password(user.pwd, hashed_password):
+        return {"result": "success"}
+
+    return {"result": "password is incorrect"}
