4482c58d19e27e6c1f4a993bee91c34774282222
choam2426 <choam2426@gmail.com>
2023-10-17 15:05:46 +0900

    collection name update

--- database/db_client.py
+++ database/db_client.py
@@ -5,9 +5,9 @@ class mongoClient:
     def __init__(self, db_server: str):
         self.client = MongoClient(db_server, serverSelectionTimeoutMS=5000)
         self.db = self.client["Boch"]
-        self.collection_auth = self.db["Auth"]
-        self.collection_users = self.db["Users"]
-        self.collection_position = self.db["Position"]
+        self.collection_auth = self.db["auth"]
+        self.collection_users = self.db["users"]
+        self.collection_position = self.db["positions"]
 
     def close_client(self):
         self.client.close()
