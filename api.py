f2eb2af6c27b8c811e190a46732001133aff8b63
choam2426 <choam2426@gmail.com>
2023-10-17 15:18:12 +0900

    debug

--- api.py
+++ api.py
@@ -28,7 +28,7 @@ async def root():
 
 
 @app.post(path="/login")
-def auth(user: model.Auth):
+def auth(user: model.auth):
     global client
     return login.auth_user(user, client.collection_auth)
 
@@ -38,48 +38,48 @@ def boch_get_user_list():
     return boch.get_boch_user_list(client.collection_users)
 
 
-@app.get(path="/boch/get/user/{user_id}")
-def boch_get_user(user_id: str = Path(..., title="user_id")):
-    return boch.get_boch_user(client.collection_users, user_id)
+@app.get(path="/boch/get/user/{user_name}")
+def boch_get_user(user_name: str = Path(..., title="user_id")):
+    return boch.get_boch_user(client.collection_users, user_name)
 
 
 @app.post(path="/boch/create/user")
-def boch_create_user(user_data: model.Users):
+def boch_create_user(user_data: model.user):
     return boch.create_boch_user(client.collection_users, user_data)
 
 
 @app.put(path="/boch/update/user")
-def boch_update_user(user_data: model.Users):
+def boch_update_user(user_data: model.user):
     return boch.update_boch_user(client.collection_users, user_data)
 
 
 @app.delete(path="/boch/delete/user")
-def boch_delete_user(user_id_list: List[str]):
-    return boch.delete_boch_user(client.collection_users, user_id_list)
+def boch_delete_user(user_name_list: List[str]):
+    return boch.delete_boch_user(client.collection_users, user_name_list)
 
 
 @app.get(path="/boch/get/positionlist")
 def boch_get_position_list():
-    return boch.get_boch_position_list(client.collection_position)
+    return boch.get_boch_position_list(client.collection_positions)
 
 
-@app.get(path="/boch/get/position/{position_id}")
-def boch_get_position(position_id: str = Path(..., title="Position ID")):
-    return boch.get_boch_position(client.collection_position, position_id)
+@app.get(path="/boch/get/position/{position_name}")
+def boch_get_position(position_name: str = Path(..., title="Position ID")):
+    return boch.get_boch_position(client.collection_positions, position_name)
 
 
 @app.post(path="/boch/create/position")
-def create_position(position: model.Position):
-    return boch.create_position(position, client.collection_position)
+def create_position(position: model.position):
+    return boch.create_position(position, client.collection_positions)
 
 
-@app.put(path="/boch/update/position/{position_id}")
+@app.put(path="/boch/update/position/{position_name}")
 def update_position(
-    position: model.Position, position_id: str = Path(..., title="Position ID")
+    position: model.Position, position_name: str = Path(..., title="Position ID")
 ):
-    return boch.update_position(position_id, position, client.collection_position)
+    return boch.update_position(position_name, position, client.collection_positions)
 
 
 @app.delete(path="/boch/delete/position")
 def delete_positions(position_id_list: List[str]):
-    return boch.delete_position(client.collection_position, position_id_list)
+    return boch.delete_position(client.collection_positions, position_id_list)
