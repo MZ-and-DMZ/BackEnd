8db2fcd8512daca11be26b1098533103e4f30a50
choam2426 <choam2426@gmail.com>
2023-10-18 17:28:58 +0900

    cors 설정

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import FastAPI, Path
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 from database import db_client
@@ -9,6 +10,14 @@ from src import boch, login
 from src.config import conf
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 
 @app.on_event("startup")
