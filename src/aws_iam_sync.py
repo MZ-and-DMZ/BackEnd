22501e8744fbc61b8b7b61088ea1bb9655a27198
choam2426 <choam2426@gmail.com>
2023-10-24 19:02:17 +0900

    솔루션 유저 생성 시 aws에 직무 적용하는 함수 완성

--- src/aws_iam_sync.py
+++ src/aws_iam_sync.py
@@ -10,17 +10,21 @@ class awsIamSync:
     def __init__(self):
         self.client = MongoClient(conf["DB_server"], serverSelectionTimeoutMS=5000)
         self.db = self.client["Boch"]
-        self.collection_positions = self.db["positions"]
         self.aws_sdk = awsSdk()
         pass
 
     # 직무를 aws iam 계정에 할당
     def position_sync_aws(self, aws_iam_user_name, position_name):
-        query_result = self.collection_positions.find_one(
-            {"positionName": position_name}
-        )
+        collection = self.db["positions"]
+        query_result = collection.find_one({"positionName": position_name})
         for policy_arn in list(query_result["policies"].value()):
             self.aws_sdk.attach_user_policy(aws_iam_user_name, policy_arn)
 
     def user_create_sync(self, user_data):
+        collection = self.db["awsUsers"]
+        query_result = collection.find_one({"userName": user_data.awsAccount})
+        if query_result is None:
+            raise 0
+        for postion_name in user_data.attachedPosition:
+            self.position_sync_aws(user_data.awsAccount, postion_name)
         pass
