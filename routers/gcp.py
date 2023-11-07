d52f8bc6623bacbb05a3ebd12e1ef4332309b713
choam2426 <choam2426@gmail.com>
2023-11-07 15:19:30 +0900

    당장 필요한 것

---
+++ routers/gcp.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from models import mongodb
+from src.util import bson_to_json
+
+router = APIRouter(prefix="/gcp", tags=["gcp"])
+
+
+@router.get(path="/role/list")
+async def get_gcp_role_list():
+    collection = mongodb.db["gcpRoles"]
+    try:
+        role_list = await collection.find().to_list(None)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    res_json = {"gcp_role_list": bson_to_json(role_list)}
+
+    return JSONResponse(content=res_json, status_code=200)
