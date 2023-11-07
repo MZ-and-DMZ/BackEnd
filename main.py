0bb107ce1e14c788bee360102e091e0b9d08dab5
choam2426 <choam2426@gmail.com>
2023-11-07 13:37:26 +0900

    auth 라우팅

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+from models import mongodb
+from routers import auth
+from src.config import conf
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+app.include_router(auth.router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    mongodb.connect(conf["db_server_url"])
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    mongodb.close()
+
+
+@app.get(path="/")
+async def root():
+    return FileResponse("./public/index.html")
+
 
 if __name__ == "__main__":
     uvicorn.run(
-        "api:app",
+        "main:app",
         host="0.0.0.0",
         port=8080,
-        workers=4,
         log_level="debug",
         reload=True,
     )
