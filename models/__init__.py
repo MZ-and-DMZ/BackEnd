94e406fef67223c5a624e3f3b09307cff7f28f69
choam2426 <choam2426@gmail.com>
2023-11-07 13:37:26 +0900

    auth 라우팅

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class MongoDB:
+    def __init__(self):
+        self.client = None
+        self.client = None
+
+    def connect(self, db_server_url):
+        self.client = AsyncIOMotorClient(db_server_url)
+        self.db = self.client["Boch"]
+
+    def close(self):
+        self.client.close()
+
+
+mongodb = MongoDB()
