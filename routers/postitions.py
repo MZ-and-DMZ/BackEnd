b18a13d349e71d7a38cb5b67a6d7ed60c1d17c1e
choam2426 <choam2426@gmail.com>
2023-11-29 13:36:06 +0900

    debug

--- routers/postitions.py
+++ routers/postitions.py
@@ -26,12 +26,12 @@ async def list_position():
             for policy in position["policies"]:
                 value = list(policy.values())[0]
                 policy_data = await awsPolicies.find_one({"_id": value})
-                policy["description"] = policy_data["Description"]
+                policy["description"] = policy_data.get("Description")
         elif position["csp"] == "gcp":
             for policy in position["policies"]:
                 value = list(policy.values())[0]
                 policy_data = await gcpRoles.find_one({"name": value})
-                policy["description"] = policy_data["description"]
+                policy["description"] = policy_data.get("Description")
 
     res_json = {"position_list": bson_to_json(position_list)}
 
