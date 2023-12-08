e80df2522b2ada089fc3a244cdb254140b92f623
Ohsaem <ss080026@naver.com>
2023-12-08 18:58:56 +0900

    롤백 api 수정

--- routers/logging/router.py
+++ routers/logging/router.py
@@ -75,60 +75,62 @@ async def get_logging_user_history(user_name: str = Path(..., title="user name")
 
 
 @router.post(path="/rollback/{user_name}")
-async def logging_rollback(version: int, user_name: str = Path(..., title="user name")):
+async def logging_rollback(version: int, csp: str, user_name: str = Path(..., title="user name")):
     try:
-        aws_sdk.iam_connect()
-        iam_client = aws_sdk.client
-        match_collection = mongodb.db["awsMatchUserAction"]
-        query_result = await match_collection.find_one({"user_name": user_name})
+        if csp == "aws":
+            aws_sdk.iam_connect()
+            iam_client = aws_sdk.client
+            aws_match_collection = mongodb.db["awsMatchUserAction"]
+            query_result = await aws_match_collection.find_one({"user_name": user_name})
 
-        # 사용자에게 연결된 정책 분리 및 삭제
-        if "attachedCustomerPolicy" in query_result:
-            current_policy_arn = query_result["attachedCustomerPolicy"]
-            # 정책을 사용자에게서 분리
-            iam_client.detach_user_policy(
-                UserName=user_name, PolicyArn=current_policy_arn
+            # 사용자에게 연결된 정책 분리 및 삭제
+            if "attachedCustomerPolicy" in query_result:
+                current_policy_arn = query_result["attachedCustomerPolicy"]
+                # 정책을 사용자에게서 분리
+                iam_client.detach_user_policy(
+                    UserName=user_name, PolicyArn=current_policy_arn
+                )
+                # 정책 삭제
+                iam_client.delete_policy(PolicyArn=current_policy_arn)
+
+            selected_record = query_result["history"][version - 1]
+            updated_version = query_result["history"][-1]["version"] + 1
+            selected_id = selected_record["action"]
+            selected_arn = selected_record["arn"]
+
+            # DB에 저장된 정책 문서 가져오기
+            collection = mongodb.db["awsCustomerPolicy"]
+            document_result = await collection.find_one({"_id": selected_arn})
+            policy_document = document_result["document"]
+            # 정책 이름 설정
+            # ARN을 ':' 문자를 기준으로 분할하고 마지막 부분을 선택
+            policy_name_split = selected_arn.split(":")[-1]
+            # '/' 문자를 기준으로 분할하고 두 번째 부분을 선택
+            policy_name = policy_name_split.split("/")[1]
+            # 고객 관리형 정책 생성
+            response = iam_client.create_policy(
+                PolicyName=policy_name, PolicyDocument=json.dumps(policy_document)
             )
-            # 정책 삭제
-            iam_client.delete_policy(PolicyArn=current_policy_arn)
+            policy_arn = response["Policy"]["Arn"]
+            # print(f"Policy Created: {policy_arn}")
+            # 사용자에게 정책 연결
+            iam_client.attach_user_policy(UserName=user_name, PolicyArn=policy_arn)
 
-        selected_record = query_result["history"][version - 1]
-        updated_version = query_result["history"][-1]["version"] + 1
-        selected_date = selected_record["date"]
-        selected_id = selected_record["action"]
-        selected_arn = selected_record["arn"]
-
-        # DB에 저장된 정책 문서 가져오기
-        collection = mongodb.db["awsCustomerPolicy"]
-        document_result = await collection.find_one({"_id": selected_arn})
-        policy_document = document_result["document"]
-        # 정책 이름 설정
-        # ARN을 ':' 문자를 기준으로 분할하고 마지막 부분을 선택
-        policy_name_split = selected_arn.split(":")[-1]
-        # '/' 문자를 기준으로 분할하고 두 번째 부분을 선택
-        policy_name = policy_name_split.split("/")[1]
-        # 고객 관리형 정책 생성
-        response = iam_client.create_policy(
-            PolicyName=policy_name, PolicyDocument=json.dumps(policy_document)
-        )
-        policy_arn = response["Policy"]["Arn"]
-        # print(f"Policy Created: {policy_arn}")
-        # 사용자에게 정책 연결
-        iam_client.attach_user_policy(UserName=user_name, PolicyArn=policy_arn)
-
-        await match_collection.update_one(
-            {"user_name": user_name},
-            {
-                "$set": {
-                    "attachedCustomerPolicy": selected_arn,
-                    "updateTime": datetime.now(),
-                    f"history.{updated_version - 1}.date": datetime.now(),
-                    f"history.{updated_version - 1}.action": selected_id,
-                    f"history.{updated_version - 1}.version": updated_version,
-                    f"history.{updated_version - 1}.arn": selected_arn,
-                }
-            },
-        )
+            await aws_match_collection.update_one(
+                {"user_name": user_name},
+                {
+                    "$set": {
+                        "attachedCustomerPolicy": selected_arn,
+                        "updateTime": datetime.now(),
+                        f"history.{updated_version - 1}.date": datetime.now(),
+                        f"history.{updated_version - 1}.action": selected_id,
+                        f"history.{updated_version - 1}.version": updated_version,
+                        f"history.{updated_version - 1}.arn": selected_arn,
+                    }
+                },
+            )
+        elif csp == "gcp":
+            gcp_match_collection = mongodb.db["gcpMatchMemberPermission"]
         return {"message": "rollback success"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
