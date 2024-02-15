3101e859ba4d1dc4495048e8d8d0a8f772642549
Ohsaem <ss080026@naver.com>
2024-02-15 11:02:17 +0900

    이상탐지 시간 및 ip 설정 수정

--- routers/anomaly_detection/router.py
+++ routers/anomaly_detection/router.py
@@ -7,6 +7,8 @@ from src.database import mongodb
 from src.utils import bson_to_json
 # from src.anomaly_detection_setting import str_to_time, time_to_str
 
+from .schemas import *
+
 router = APIRouter(prefix="/anomaly/detection", tags=["anomaly detection"])
 
 
@@ -17,7 +19,7 @@ async def time_list():
         time_list = await collection.find({}).to_list(None)
 
         for item in time_list:
-            item.pop("_id")
+            item["group"] = item.pop("_id")
             # item["startTime"] = time_to_str(item["startTime"])
             # item["endTime"] = time_to_str(item["endTime"])
     except Exception as e:
@@ -29,16 +31,17 @@ async def time_list():
 
 
 @router.post(path="/time/set")
-async def set_time(group: str, start_time: str, end_time: str):
+async def set_time(working_hours: workingHours):
     collection = mongodb.db["anomalyDetectionTime"]
+    insert_data = working_hours.dict()
+    insert_data["_id"] = insert_data.pop("group")
+
     try:
         # 시작 시간과 종료 시간을 파싱하여 datetime 객체로 변환
         # start_time = str_to_time(start_time)
         # end_time = str_to_time(end_time)
 
-        query_result = await collection.insert_one(
-            {"group": group, "startTime": start_time, "endTime": end_time}
-        )
+        query_result = await collection.insert_one(insert_data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -49,6 +52,19 @@ async def set_time(group: str, start_time: str, end_time: str):
         )
     else:
         raise HTTPException(status_code=500, detail="failed to set")
+    
+
+@router.delete(path="/time/delete")
+async def delete_time(group: str):
+    collection = mongodb.db["anomalyDetectionTime"]
+    try:
+        delete_result = await collection.delete_one({"_id": group})  # 삭제
+        if delete_result.deleted_count == 1:
+            return {"message": "ip delete success"}
+        else:
+            raise HTTPException(status_code=500, detail="deletion failed")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get(path="/iplist")
@@ -58,7 +74,7 @@ async def ip_list():
         ip_list = await collection.find({}).to_list(None)
 
         for item in ip_list:
-            item.pop("_id")
+            item["ip"] = item.pop("_id")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -68,12 +84,13 @@ async def ip_list():
 
 
 @router.post(path="/ip/set")
-async def set_ip(ip: str):
+async def set_ip(working_ip: workingIP):
     collection = mongodb.db["anomalyDetectionIP"]
+    insert_data = working_ip.dict()
+    insert_data["_id"] = insert_data.pop("ip")
+
     try:
-        query_result = await collection.insert_one(
-            {"IP": ip}
-        )
+        query_result = await collection.insert_one(insert_data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -84,3 +101,16 @@ async def set_ip(ip: str):
         )
     else:
         raise HTTPException(status_code=500, detail="failed to set")
+
+
+@router.delete(path="/ip/delete")
+async def delete_time(ip: str):
+    collection = mongodb.db["anomalyDetectionIP"]
+    try:
+        delete_result = await collection.delete_one({"_id": ip})  # 삭제
+        if delete_result.deleted_count == 1:
+            return {"message": "time delete success"}
+        else:
+            raise HTTPException(status_code=500, detail="deletion failed")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
