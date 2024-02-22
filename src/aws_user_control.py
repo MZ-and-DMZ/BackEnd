2f7d21199848a922cd8a8f73747a7f7590fbef6b
choam2426 <choam2426@gmail.com>
2024-02-22 14:47:51 +0900

    디렉토리 추가

--- src/aws_user_control.py
+++ src/aws_user_control.py
@@ -2,7 +2,15 @@ from src.database import mongodb
 
 from .aws_sdk_connect import aws_sdk
 
+
 async def create_user(user_name: str):
     aws_sdk.iam_connect()
     aws_sdk.client.create_user(UserName=user_name)
+    aws_sdk.iam_close()
+
+
+async def attach_mfa_policy(user_name: str):
+    arn = "arn:aws:iam::481280410531:policy/forceMfa"
+    aws_sdk.iam_connect()
+    aws_sdk.client.attach_user_policy(UserName=user_name, PolicyArn=arn)
     aws_sdk.iam_close()
